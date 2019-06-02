5c2844f61c921c722834ee5bacd6b4be2b7766c3
joachimprinzbach <joachim.prinzbach@gmail.com>
2019-06-02 10:38:19 +0200

    Improve gitignore for IntelliJ Usage
    - add basic header and footer

--- immo-control-client/src/app/app.component.ts
+++ immo-control-client/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'imc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'immo-control-client';
 }
